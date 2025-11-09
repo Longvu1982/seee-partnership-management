17b218ef49fe39151b5ebc36b9450fec23b531ec
Long Vu (Kris Nguyen) <89139559+Longvu1982@users.noreply.github.com>
2025-11-10 02:12:39 +0700

    fix(error-handler): improve error message handling and display in toast notifications
    - Added AxiosError check to standardize error messages.
    - Updated toast notification description to use a styled span for better visibility.

--- frontend/src/hooks/use-error-handler.tsx
+++ frontend/src/hooks/use-error-handler.tsx
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import { AlertTriangle } from "lucide-react";
 import { toast } from "sonner";
 
@@ -6,11 +7,15 @@ export const useErrorHandler = () => {
     let message = error?.response?.data?.error?.message;
     if (typeof message === "object") message = "Lỗi bất định hoặc lỗi server";
 
+    if (error instanceof AxiosError) message = "Lỗi bất định hoặc lỗi server";
+
     message = message ?? error?.message;
 
     toast.error("Có lỗi xảy ra!", {
       icon: <AlertTriangle className="text-orange-600 mr-2" size={20} />,
-      description: message,
+      description: (
+        <span className="text-black dark:text-white">{message}</span>
+      ),
       duration: 2000,
     });
   };
